5692c486a2d3b9cb2e14cc8dbec54e9c50b6f52c
Isheka123 <kavyasaiisheka.y20@iiits.in>
2025-03-23 19:00:20 +0530

    version 2

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Navbar from "../navbar/Navbar";
 const SmartBotProductPage = () => {
   return (
     <div className="max-w-6xl mx-auto p-6">
+         <Navbar />
       <h1 className="text-4xl font-bold text-center mb-6">SMART BOT</h1>
       <p className="text-lg text-center text-gray-600 mb-10">
         The Future of Interaction Today
